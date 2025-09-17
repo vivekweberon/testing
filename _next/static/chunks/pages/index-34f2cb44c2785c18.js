387fd689c1198eb4242d958e99824e4f903375d0
Developer-Weberon <developer@weberon.net>
2025-09-17 06:52:10 +0000

    Automated Commit from the Jenkins build: https://cdi-dev.weberon.net/job/Reachppersona/job/Build-Deploy-ReachPersona-NextJS-Website/261/

--- _next/static/chunks/pages/index-34f2cb44c2785c18.js
+++ _next/static/chunks/pages/index-34f2cb44c2785c18.js
@@ -13,7 +13,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 const index = props => {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_templates_WebsitePage__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, props));
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Testing"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_templates_WebsitePage__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, props));
 };
 var __N_SSG = true;
 /* harmony default export */ __webpack_exports__["default"] = (index);
